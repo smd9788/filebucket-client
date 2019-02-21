42892fd6b0f918b3c8bf997afc6b853f6ca1bd4b
andywu0489 <andywu0489@gmail.com>
2019-02-20 21:46:04 -0500

    Add features to view all uploads and user uploads
    -Add new button in index.html to view only user owned uploads
    -Add new event listener in app
    -Create new get-all-uploads.handlebars file to display all uploads
    -Add appropriate code in api, events, and ui files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,4 +18,5 @@ $(() => {
   $('#delete-upload').on('submit', uploadEvents.onDeleteUpload)
   $('body').on('click', '#delete', uploadEvents.onHandlebarDelete)
   $('body').on('submit', '#edit-upload-modal-form', uploadEvents.onUpdateUpload)
+  $('#mystuff').on('click', uploadEvents.onGetMyStuff)
 })
